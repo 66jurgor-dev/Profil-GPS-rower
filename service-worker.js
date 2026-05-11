1ff5614e37472e87f93b4544a7496801a9c49cd7
66jurgor-dev <66jurgor@gmail.com>
2026-05-11 11:03:39 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'profil-gps-pwa-v75-cache-first-mountains';
+const CACHE_NAME = 'profil-gps-pwa-v95-forecast-push-ahead';
 const FILES = ['index.html','manifest.json','service-worker.js','trasa.js','icon.svg','icon-192.png','icon-512.png'];
 self.addEventListener('install', e => {
   self.skipWaiting();
